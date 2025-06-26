8f947262242fb3476a55b27a5f02045f10616ae7
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2025-06-26 13:09:07 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+color: red;
+text-align: center;
+`;
+
+function App() {
+
+  return (
+    <>
+      <Title>Portfolio</Title>
+    </>
+  )
+}
+
+export default App

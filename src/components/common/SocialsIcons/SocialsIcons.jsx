b8061f1fa6df8e8d6ee9fbd6e1ef958fe3eef0e5
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2025-07-18 14:27:10 +0200

    refactor SocialsIcons component; extract social data into separate SocialsIconsData file for improved maintainability

--- src/components/common/SocialsIcons/SocialsIcons.jsx
+++ src/components/common/SocialsIcons/SocialsIcons.jsx
@@ -1,31 +1,19 @@
 import { SocialItem, SocialLink, SocialIcon } from './styles';
+import { socials } from './SocialsIconsData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faGithub, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 const SocialsIcons = ({ isFooter = false }) => {
     return (
         <>
-            <SocialItem>
-                <SocialLink href="https://www.linkedin.com/in/sebastian-juszczynski-52982a243/" target="_blank"
-                    aria-label="LinkedIn">
-                    <SocialIcon $isFooter={isFooter}><FontAwesomeIcon icon={faLinkedinIn} /></SocialIcon>
-                </SocialLink>
-            </SocialItem>
-            <SocialItem>
-                <SocialLink href="https://github.com/sebastianjuszczynski" target="_blank" aria-label="Github">
-                    <SocialIcon $isFooter={isFooter}><FontAwesomeIcon icon={faGithub} /></SocialIcon>
-                </SocialLink>
-            </SocialItem>
-            <SocialItem>
-                <SocialLink href="https://www.facebook.com/sebastian.juszczynski.3/" target="_blank" aria-label="Facebook">
-                    <SocialIcon $isFooter={isFooter}><FontAwesomeIcon icon={faFacebookF} /></SocialIcon>
-                </SocialLink>
-            </SocialItem>
-            <SocialItem>
-                <SocialLink href="https://www.instagram.com/sjuszczynski/" target="_blank" aria-label="Instagram">
-                    <SocialIcon $isFooter={isFooter}><FontAwesomeIcon icon={faInstagram} /></SocialIcon>
-                </SocialLink>
-            </SocialItem>
+            {socials.map(({ href, label, icon }) => (
+                <SocialItem key={label}>
+                    <SocialLink href={href} target="_blank"
+                        aria-label={label}>
+                        <SocialIcon $isFooter={isFooter}><FontAwesomeIcon icon={icon} /></SocialIcon>
+                    </SocialLink>
+                </SocialItem>
+            ))}
         </>
     );
 };
---
+++ src/components/common/SocialsIcons/SocialsIconsData.js
@@ -0,0 +1,24 @@
+import { faLinkedinIn, faGithub, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+    export const socials = [
+        {
+            href: "https://www.linkedin.com/in/sebastian-juszczynski-52982a243/",
+            label: "LinkedIn",
+            icon: faLinkedinIn
+        },
+        {
+            href: "https://github.com/sebastianjuszczynski",
+            label: "Github",
+            icon: faGithub
+        },
+        {
+            href: "https://www.facebook.com/sebastian.juszczynski.3/",
+            label: "Facebook",
+            icon: faFacebookF
+        },
+        {
+            href: "https://www.instagram.com/sjuszczynski/",
+            label: "Instagram",
+            icon: faInstagram
+        }
+
+    ];
